4694a0a0f4757daa99f9564bd366e844c6a49600
Manasa PVS <venkatasrimanasa.p18@iiits.in>
2021-05-18 21:28:35 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/u6amtech/resturant_delivery_boy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.u6amtech.resturant_delivery_boy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utill/images.dart
@@ -0,0 +1,25 @@
+class Images {
+  // for Icon
+  static const String efood = 'assets/icon/efood.png';
+  static const String efood_bike = 'assets/icon/efood_bike.png';
+  static const String search = 'assets/icon/search.png';
+  static const String done_with_full_background = 'assets/icon/done_with_full_background.png';
+  static const String location = 'assets/icon/location.png';
+
+  // for Image
+  static const String arabic = 'assets/image/arabic.png';
+  static const String china = 'assets/image/china.png';
+  static const String germany = 'assets/image/germany.png';
+  static const String italy = 'assets/image/italy.png';
+  static const String japan = 'assets/image/japan.png';
+  static const String korea = 'assets/image/korean.png';
+  static const String united_kindom = 'assets/image/united_kindom.png';
+  static const String efood_bike_with_person = 'assets/icon/efood_bike_with_person.png';
+  static const String done = 'assets/icon/done.png';
+  static const String log_out = 'assets/image/log_out.png';
+  static const String placeholder_image = 'assets/image/placeholder_image.png';
+  static const String placeholder_banner = 'assets/image/placeholder_banner.png';
+  static const String placeholder_rectangle = 'assets/image/placeholder_rectangle.png';
+  static const String placeholder_user = 'assets/image/placeholder_user.png';
+  static const String money = 'assets/image/money.png';
+}
