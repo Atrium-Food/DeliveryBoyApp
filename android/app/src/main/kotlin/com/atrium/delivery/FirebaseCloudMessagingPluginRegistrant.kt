2e551d6821979329570527bcb00fea5990bf163c
Manasa PVS <venkatasrimanasa.p18@iiits.in>
2021-06-07 21:58:50 +0530

    Home screen: Delivery App

--- android/app/src/main/kotlin/com/atrium/delivery/FirebaseCloudMessagingPluginRegistrant.kt
+++ android/app/src/main/kotlin/com/atrium/delivery/FirebaseCloudMessagingPluginRegistrant.kt
@@ -1,4 +1,4 @@
-package com.u6amtech.resturant_delivery_boy
+package com.atrium.delivery
 
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingPlugin
